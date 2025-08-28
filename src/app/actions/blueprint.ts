c7463f1766087c3d846d0b48f2fb791508905a68
Dwight Ware <dwightware4@gmail.com>
2025-08-28 13:55:19 -0700

    Initial hookup to AI SDK

--- src/app/actions/blueprint.ts
+++ src/app/actions/blueprint.ts
@@ -1,12 +1,26 @@
 'use server'
 
+import { openai } from '@ai-sdk/openai'
+import { generateText } from 'ai'
+
 export const create = async (formData: any) => {
   const name = formData.get('name')
   const email = formData.get('email')
   const website = formData.get('website')
-  console.log('---------------------------------')
 
-  console.log({ name, email, website })
+  console.log('----------------------------')
+  console.log('Starting...')
+  console.log('----------------------------')
 
-  console.log('---------------------------------')
+  const { text } = await generateText({
+    model: openai('gpt-5-nano'),
+    system:
+    'You are a professional writer. '
+    + 'You write simple, clear, and concise content.',
+    prompt: `Make up a 2-3 sentence story about the owner of the following company: ${name}, ${email}, ${website}`,
+  })
+
+  console.log('----------------------------')
+  console.log(text)
+  console.log('----------------------------')
 }
