2e13ff7c8da2c510fee5359de64a9e02b2761433
Dwight Ware <dwightware4@gmail.com>
2025-08-28 15:17:56 -0700

    Prompt tweak

--- src/app/actions/blueprint.ts
+++ src/app/actions/blueprint.ts
@@ -3,13 +3,13 @@
 import { openai } from '@ai-sdk/openai'
 import { generateText } from 'ai'
 
-export const create = async ({ name, email, website }: any) => {
+export const create = async ({ name, website }: any) => {
   const { text } = await generateText({
     model: openai('gpt-5-nano'),
     system:
     'You are a professional writer. '
     + 'You write simple, clear, and concise content. No notes or disclaimers.',
-    prompt: `Make up a 2-3 sentence story about the owner of the following company: ${name}, ${email}, ${website}`,
+    prompt: `Make up a 2-3 sentence story about the ${name}, the owner of the company with the website ${website}`,
   })
 
   return { success: true, text }
