9d1d736329fcc81e41a65011f52df8f234f9bb7a
Dwight Ware <dwightware4@gmail.com>
2025-08-28 15:12:06 -0700

    Simple AI integration

--- src/app/actions/blueprint.ts
+++ src/app/actions/blueprint.ts
@@ -3,24 +3,14 @@
 import { openai } from '@ai-sdk/openai'
 import { generateText } from 'ai'
 
-export const create = async (formData: any) => {
-  const name = formData.get('name')
-  const email = formData.get('email')
-  const website = formData.get('website')
-
-  console.log('----------------------------')
-  console.log('Starting...')
-  console.log('----------------------------')
-
+export const create = async ({ name, email, website }: any) => {
   const { text } = await generateText({
     model: openai('gpt-5-nano'),
     system:
     'You are a professional writer. '
-    + 'You write simple, clear, and concise content.',
+    + 'You write simple, clear, and concise content. No notes or disclaimers.',
     prompt: `Make up a 2-3 sentence story about the owner of the following company: ${name}, ${email}, ${website}`,
   })
 
-  console.log('----------------------------')
-  console.log(text)
-  console.log('----------------------------')
+  return { success: true, text }
 }
--- src/app/components/BlueprintButton.tsx
+++ src/app/components/BlueprintButton.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { Checkbox, Group } from '@mantine/core'
+
 import { isEmail, isNotEmpty, matches } from '@mantine/form'
+import { useActionState, useState } from 'react'
 import { Button, Modal, TextInput } from '@/components'
 import { useDisclosure, useForm } from '@/hooks'
 
@@ -10,12 +12,14 @@ import { create } from '../actions/blueprint'
 export const BlueprintButton = () => {
   const [opened, { open, close }] = useDisclosure(false)
 
+  const [loading, setLoading] = useState(false)
+
   const form = useForm({
     mode: 'uncontrolled',
     initialValues: {
-      name: 'Dwight Ware',
-      email: 'dwight.ware@example.com',
-      website: 'https://www.dwightware.com',
+      name: '',
+      email: '',
+      website: '',
       auditPermission: false,
     },
     validate: {
@@ -27,50 +31,59 @@ export const BlueprintButton = () => {
     onSubmitPreventDefault: 'validation-failed',
   })
 
+  const [state, createAction] = useActionState(create, form.getValues() as any)
+
   return (
     <>
       <Modal opened={opened} onClose={close} title="Enter Your Details">
-        <form action={create} onSubmit={form.onSubmit(() => { })}>
-          <TextInput
-            withAsterisk
-            label="Name"
-            placeholder="Your Name"
-            key={form.key('name')}
-            name="name"
-            {...form.getInputProps('name')}
-          />
+        {state.success && (<div>{state.text}</div>)}
 
-          <TextInput
-            mt="lg"
-            withAsterisk
-            label="Email"
-            placeholder="your@email.com"
-            key={form.key('email')}
-            name="email"
-            {...form.getInputProps('email')}
-          />
+        {!state.success && loading && <div>Loading...</div>}
 
-          <TextInput
-            mt="lg"
-            withAsterisk
-            label="Website"
-            placeholder="yourwebsite.com"
-            key={form.key('website')}
-            name="website"
-            {...form.getInputProps('website')}
-          />
+        {!loading && (
+          <form action={createAction} onSubmit={form.onSubmit(() => { setLoading(true) })}>
+            <TextInput
+              withAsterisk
+              label="Name"
+              placeholder="Your Name"
+              key={form.key('name')}
+              name="name"
+              {...form.getInputProps('name')}
+            />
 
-          <Checkbox
-            mt="xl"
-            label="I give permission to QOLA360 Convert to analyze my site and ads for audit purposes."
-            key={form.key('auditPermission')}
-            {...form.getInputProps('auditPermission', { type: 'checkbox' })}
-          />
+            <TextInput
+              mt="lg"
+              withAsterisk
+              label="Email"
+              placeholder="your@email.com"
+              key={form.key('email')}
+              name="email"
+              {...form.getInputProps('email')}
+            />
 
-          <Group justify="flex-end" mt="md">
-            <Button type="submit">Submit</Button>
-          </Group>
-        </form>
+            <TextInput
+              mt="lg"
+              withAsterisk
+              label="Website"
+              placeholder="yourwebsite.com"
+              key={form.key('website')}
+              name="website"
+              {...form.getInputProps('website')}
+            />
+
+            <Checkbox
+              mt="xl"
+              label="I give permission to QOLA360 Convert to analyze my site and ads for audit purposes."
+              key={form.key('auditPermission')}
+              {...form.getInputProps('auditPermission', { type: 'checkbox' })}
+            />
+
+            <Group justify="flex-end" mt="md">
+              <Button type="submit">Submit</Button>
+            </Group>
+          </form>
+        )}
+
       </Modal>
 
       <Button onClick={open}>
