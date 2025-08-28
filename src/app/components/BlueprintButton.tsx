abc6b2d65004ebd5bb0380e4b2521cc3dafc3dae
Dwight Ware <dwightware4@gmail.com>
2025-08-28 12:18:29 -0700

    Got basic form handling working

--- src/app/components/BlueprintButton.tsx
+++ src/app/components/BlueprintButton.tsx
@@ -1,15 +1,71 @@
 'use client'
 
-import { Button, Modal } from '@/components'
-import { useDisclosure } from '@/hooks'
+import { Checkbox, Group } from '@mantine/core'
+import { isEmail, isNotEmpty, matches } from '@mantine/form'
+import { Button, Modal, TextInput } from '@/components'
+import { useDisclosure, useForm } from '@/hooks'
 
 export const BlueprintButton = () => {
   const [opened, { open, close }] = useDisclosure(false)
 
+  const form = useForm({
+    mode: 'uncontrolled',
+    initialValues: {
+      name: 'Dwight Ware',
+      email: 'dwight.ware@example.com',
+      website: 'https://www.dwightware.com',
+      auditPermission: false,
+    },
+
+    validate: {
+      name: isNotEmpty('Name is required'),
+      email: isEmail('Invalid email'),
+      website: matches(/^(?:https?:\/\/)?(?:www\.)?[-\w@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-\w()@:%+.~#?&/=]*$/, 'Invalid website'),
+      auditPermission: isNotEmpty('Permission is required'),
+    },
+  })
+
   return (
     <>
-      <Modal opened={opened} onClose={close}>
-        Hello, I'm a modal!
+      <Modal opened={opened} onClose={close} title="Enter Your Details">
+        <form onSubmit={form.onSubmit(values => console.log(values))}>
+          <TextInput
+            withAsterisk
+            label="Name"
+            placeholder="Your Name"
+            key={form.key('name')}
+            {...form.getInputProps('name')}
+          />
+
+          <TextInput
+            mt="lg"
+            withAsterisk
+            label="Email"
+            placeholder="your@email.com"
+            key={form.key('email')}
+            {...form.getInputProps('email')}
+          />
+
+          <TextInput
+            mt="lg"
+            withAsterisk
+            label="Website"
+            placeholder="yourwebsite.com"
+            key={form.key('website')}
+            {...form.getInputProps('website')}
+          />
+
+          <Checkbox
+            mt="xl"
+            label="I give permission to QOLA360 Convert to analyze my site and ads for audit purposes."
+            key={form.key('auditPermission')}
+            {...form.getInputProps('auditPermission', { type: 'checkbox' })}
+          />
+
+          <Group justify="flex-end" mt="md">
+            <Button type="submit">Submit</Button>
+          </Group>
+        </form>
       </Modal>
 
       <Button onClick={open}>
