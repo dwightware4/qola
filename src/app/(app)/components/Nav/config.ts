a169c930097eae30d15c47b16936358b96d9339a
Dwight Ware <dwightware4@gmail.com>
2025-08-29 15:07:12 -0700

    Update navigation icons for LP Analysis, Offer Blueprint, ICP, and Gap Analysis; import necessary icons from heroicons

--- src/app/(app)/components/Nav/config.ts
+++ src/app/(app)/components/Nav/config.ts
@@ -1,6 +1,6 @@
 import type { ComponentTypeT, SVGPropsT, SVGSVGElementT } from '@/types'
 import { GAP_ANALYSIS_ROUTE, ICP_ROUTE, LP_ANALYSIS_ROUTE, OFFER_BLUEPRINT_ROUTE } from '@/core'
-import { WrenchScrewdriverIcon } from '@/icons'
+import { ChartBarIcon, DocumentMagnifyingGlassIcon, MapIcon, UserIcon } from '@/icons'
 
 type NavItemT = {
   name: string
@@ -10,8 +10,8 @@ type NavItemT = {
 export type NavItemsT = NavItemT[]
 
 export const NAV_ITEMS: NavItemsT = [
-  { name: 'LP Analysis', href: LP_ANALYSIS_ROUTE, icon: WrenchScrewdriverIcon },
-  { name: 'Offer Blueprint', href: OFFER_BLUEPRINT_ROUTE, icon: WrenchScrewdriverIcon },
-  { name: 'ICP', href: ICP_ROUTE, icon: WrenchScrewdriverIcon },
-  { name: 'Gap Analysis', href: GAP_ANALYSIS_ROUTE, icon: WrenchScrewdriverIcon },
+  { name: 'LP Analysis', href: LP_ANALYSIS_ROUTE, icon: ChartBarIcon },
+  { name: 'Offer Blueprint', href: OFFER_BLUEPRINT_ROUTE, icon: MapIcon },
+  { name: 'Ideal Customer Profile', href: ICP_ROUTE, icon: UserIcon },
+  { name: 'Gap Analysis', href: GAP_ANALYSIS_ROUTE, icon: DocumentMagnifyingGlassIcon },
 ]
--- src/icons/index.ts
+++ src/icons/index.ts
@@ -2,8 +2,12 @@ export {
   ArrowUpOnSquareIcon,
   Bars3Icon,
   BookOpenIcon,
+  ChartBarIcon,
   CheckCircleIcon,
+  DocumentMagnifyingGlassIcon,
+  MapIcon,
   PhotoIcon,
+  UserIcon,
   WrenchScrewdriverIcon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
